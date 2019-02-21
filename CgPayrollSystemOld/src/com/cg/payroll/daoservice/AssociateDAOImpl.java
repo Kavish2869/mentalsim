3b2a43052f046f28299d74514886b56dd77280c4
Kavish2869 <47375814+Kavish2869@users.noreply.github.com>
2019-02-21 16:04:16 +0530

    Add files via upload

---
+++ CgPayrollSystemOld/src/com/cg/payroll/daoservice/AssociateDAOImpl.java
@@ -0,0 +1,39 @@
+package com.cg.payroll.daoservice;
+import com.cg.payroll.beans.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cg.payroll.beans.Associate;
+import com.cg.payroll.util.PayrollDBUtil;
+import com.cg.payroll.beans.*;
+public class AssociateDAOImpl implements AssociateDAO {
+
+	@Override
+	public Associate save(Associate associate) {
+		// TODO Auto-generated method stub
+		associate.setAssociateId(PayrollDBUtil.getASSOCIATE_ID_COUNTER());
+		PayrollDBUtil.associates.put(associate.getAssociateId(),associate );
+		return associate;
+	}
+
+	@Override
+	public boolean update(Associate associate) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Associate findOne(int  associateId) {
+		// TODO Auto-generated method stub
+		return PayrollDBUtil.associates.get(associateId);
+					
+	}
+		@Override
+	public List<Associate> findAll() {
+		// TODO Auto-generated method stub
+		ArrayList<Associate> associateList = new ArrayList<>(PayrollDBUtil.associates.values());
+		
+		return associateList;
+	}
+}
